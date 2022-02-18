4130f5a6a27935d40649b1851c1d8d76f7e56a17
Jeerawat Odds <jeerawat@odds.team>
2022-02-18 23:31:46 +0700

    [Create] UserController.login issue #6

---
+++ Shopping/src/main/java/com/example/Shopping/User/ResponseLogin.java
@@ -0,0 +1,6 @@
+package com.example.Shopping.User;
+
+import com.example.Shopping.utility.ApiResponse;
+
+public class ResponseLogin extends ApiResponse<UserModel> {
+}
---
+++ Shopping/src/main/java/com/example/Shopping/User/UserController.java
@@ -0,0 +1,31 @@
+package com.example.Shopping.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService m_userService;
+
+    @PostMapping("/api/v1/login")
+    public ResponseLogin login(@RequestBody RequestLogin p_body) {
+        UserModel modelUser = m_userService.getUserByUsername(p_body.getUsername());
+        boolean bolIsEqual = m_userService.isEqualPassword(modelUser, p_body.getPassword());
+        ResponseLogin response = new ResponseLogin();
+
+        if(bolIsEqual) {
+            response.setData(modelUser);
+
+        } else {
+            response.setHttpStatus(HttpStatus.NOT_FOUND);
+            response.setMessage(String.format("User: %s not found!", p_body.getUsername()));
+        }
+
+        return response;
+    }
+}
