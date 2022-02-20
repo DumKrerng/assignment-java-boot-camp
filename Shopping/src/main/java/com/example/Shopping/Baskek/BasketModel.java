e34db52a73ba768e14cbde8738dba320e8056261
Jeerawat Odds <jeerawat@odds.team>
2022-02-20 12:18:39 +0700

    [Create] BasketItemRepository, BasketItemRepository issue #11

---
+++ Shopping/src/main/java/com/example/Shopping/Baskek/BasketModel.java
@@ -0,0 +1,34 @@
+package com.example.Shopping.Baskek;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class BasketModel {
+
+	@Id
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator(name = "uuid", strategy = "uuid2")
+	private String basketID;
+
+	private String basketStatus;
+
+	public void setBasketStatus(String basketStatus){
+		this.basketStatus = basketStatus;
+	}
+
+	public String getBasketStatus(){
+		return basketStatus;
+	}
+
+	public void setBasketID(String basketID){
+		this.basketID = basketID;
+	}
+
+	public String getBasketID(){
+		return basketID;
+	}
+}
---
+++ Shopping/src/main/java/com/example/Shopping/BasketItem/BasketItemModel.java
@@ -0,0 +1,56 @@
+package com.example.Shopping.BasketItem;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class BasketItemModel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int basketItemID;
+	private String basketID;
+	private String productID;
+	private double unitPrice;
+	private int quantity;
+
+	public void setUnitPrice(double p_dubUnitPrice){
+		this.unitPrice = p_dubUnitPrice;
+	}
+
+	public double getUnitPrice(){
+		return unitPrice;
+	}
+
+	public void setQuantity(int p_intQuantity){
+		this.quantity = p_intQuantity;
+	}
+
+	public int getQuantity(){
+		return quantity;
+	}
+
+	public void setProductID(String p_strProductID){
+		this.productID = p_strProductID;
+	}
+
+	public String getProductID(){
+		return productID;
+	}
+
+	public void setBasketID(String p_strBasketID){
+		this.basketID = p_strBasketID;
+	}
+
+	public String getBasketID(){
+		return basketID;
+	}
+
+	public void setBasketItemID(int p_intBasketItemID){
+		this.basketItemID = p_intBasketItemID;
+	}
+
+	public int getBasketItemID(){
+		return basketItemID;
+	}
+}
